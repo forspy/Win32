a6aeb38b2a02ffb7ffd105bc180663d06343c58f
forspy <qiubow@126.com>
2019-01-15 13:32:26 +0800

    教学_win32_GDI2_画刷_字体_位图

--- teach_win32_GDI 2.cpp
+++ teach_win32_GDI 2.cpp
@@ -120,7 +120,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_PAINT    - 绘制主窗口
 //  WM_DESTROY  - 发送退出消息并返回
 //
-//GDI2 --画刷
+//GDI2 --画刷、文字、位图bmp
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
@@ -133,6 +133,32 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		位图包含：文件头、信息头、彩色表、图像数据库阵列
 		*/
 		//png为矢量图 记录一个起点 终点 然后这个区间内颜色变化的规律，画的时候以计算 然后渲染 一般可压缩 缩放不失真
+		//位图按照一层一层画上去比如有10张图就有10层
+		HDC hdc = GetDC(hWnd);//设备环境句柄
+		HDC srcDc1 = CreateCompatibleDC(hdc);//创建一个辅助DC把资源加载进来，因为要一层一层的画
+		//添加资源：在资源视图里面添加 方法：右键项目 添加资源 bitmap 选择新建 查看属性优先设置为24位以上
+		//然后使用照片查看器打开 ctrl c 然后在资源位图里面ctrl v
+		//左键点击图片就可以看到属性，需要在选择状态下
+		HBITMAP srcBitmap1 = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP1));//从资源加载一张位图
+		SelectObject(srcDc1, srcBitmap1);//把srcBitmap1关联进srcDc1里面
+		BitBlt(hdc,0,0,1024,768,srcDc1,0,0,SRCCOPY);//层次绘图 为什么一放大就没了
+		DeleteObject(srcBitmap1);//释放位图对象
+		DeleteDC(srcDc1);//释放辅助DC
+		ReleaseDC(hWnd, hdc);
+		/*
+		BOOL BitBlt(
+  _In_ HDC   hdcDest,
+  _In_ int   nXDest,
+  _In_ int   nYDest,
+  _In_ int   nWidth,
+  _In_ int   nHeight,
+  _In_ HDC   hdcSrc,
+  _In_ int   nXSrc,
+  _In_ int   nYSrc,
+  _In_ DWORD dwRop
+);
+		*/
+		
 	}
 	break;
 	case WM_LBUTTONDBLCLK://左键双击  在注册窗口类 需要加上CS_DBCLKS才能响应双击的操作
@@ -157,9 +183,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		给一个值去字体库取出图片打印
 		用户输入一个消息一个值，字体安装软件截获这个消息，并引导windows系统去外接字体库寻找
 		*/
-		SelectObject(hdc, hfont);
-		SetTextColor(hdc, 0xff);
-		SetBkColor(hdc, 0xff0000);
+		SelectObject(hdc, hfont);//把字体句柄关联进hdc设备环境句柄
+		SetTextColor(hdc, 0xff00);//设置文字颜色
+		SetBkMode(hdc, TRANSPARENT);//设置文字背景模式 OPAQUE为白色，TRANSPARENT为文字背景透明
+		//SetBkColor(hdc, 0xff0000);//设置文字背景颜色
 		TextOut(hdc, 300, 100, _T("hell world!"), 11);//锚点默认在矩形左上角
 		
 		DeleteObject(hfont);
@@ -207,24 +234,42 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_KEYDOWN:
 	{
 		//GDI-画刷
-		HDC hdc = GetDC(hWnd);
+		HDC hdc = GetDC(hWnd);//设备环境句柄
 		HBRUSH hbr;//画刷句柄
-				   //hbr = (HBRUSH)GetStockObject(BLACK_BRUSH);//使用系统定义的颜色
-				   //hbr = CreateSolidBrush(0xff);//类似于hpen的自定义颜色,纯色画刷
-		//hbr = CreateHatchBrush(HS_HORIZONTAL, 0xff);//阴影画刷
-		HBITMAP hbt = (HBITMAP)LoadImage(hInst, _T("girl.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+		//hbr = (HBRUSH)GetStockObject(BLACK_BRUSH);//使用系统定义的颜色
+		//hbr = CreateSolidBrush(0xff);//类似于hpen的自定义颜色,A.纯色画刷
+		//hbr = CreateHatchBrush(HS_HORIZONTAL, 0xff);//B.阴影画刷
+		HBITMAP hbt = (HBITMAP)LoadImage(hInst, _T("pat.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 		//加载图片GDI支持bmp 不支持jpg 注意jpg改后缀不行 因为位图bmp包含：文件头、信息头、彩色表、图像数据库阵列与jpg不一样 可以使用 画图 另存为bmp
-		hbr = CreatePatternBrush(hbt);
+		hbr = CreatePatternBrush(hbt);//C.位图画刷
 		SelectObject(hdc, hbr);//把画刷关联进hdc设备环境句柄
-							   //---改变边框画笔
-		Ellipse(hdc, 100, 100, 600, 400);
-		/*HPEN hpen = CreatePen(PS_SOLID, 1, 0xff);
-		SelectObject(hdc, hpen);*/
-		//-----------
+
+		//---改变边框画笔
+		HPEN hpen = CreatePen(PS_SOLID, 1, 0xff);
+		SelectObject(hdc, hpen);
+		//---边框选择
+		//Ellipse(hdc, 100, 100, 600, 400);//圆或椭圆 坐标为外接矩形
 		//Rectangle(hdc, 100, 100, 400, 400);//画矩形,默认白色画刷填充，黑色画笔画边框
-		//DeleteObject(hpen);
-		DeleteObject(hbr);
-		ReleaseDC(hWnd, hdc);
+		//RoundRect(hdc, 100, 100, 600, 400, 300, 200);//画圆角矩形，最后两个参数为圆角弧
+		//Pie(hdc,100,100,400,400,0,0,250,400);//画饼图 外接矩形定位，弧的画法和圆弧画法一样为点到圆心的连线的延长线于圆的交点 方向为逆时针画法
+		Chord(hdc, 100, 100, 400, 400, 0, 0, 250, 400);//画弦图，参数与饼图一样
+		/*
+		BOOL Pie(
+  HDC hdc,
+  int left,外接矩形区域
+  int top,外接矩形区域
+  int right,外接矩形区域
+  int bottom,外接矩形区域
+  int xr1,
+  int yr1,
+  int xr2,
+  int yr2
+);
+		*/
+		//------
+		DeleteObject(hpen);//释放画笔对象
+		DeleteObject(hbr);//释放画刷对象
+		ReleaseDC(hWnd, hdc);//释放设备环境句柄
 	}
 	break;
     case WM_COMMAND:
