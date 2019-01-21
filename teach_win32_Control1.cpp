1b789bb1f12f3d8a05522d61b64ccb6e5824e485
forspy <qiubow@126.com>
2019-01-21 17:01:36 +0800

    教学_win32_阻塞/非阻塞对话框_控件_下压式按钮_组框_编辑框_调察问卷

--- teach_win32_Control1.cpp
+++ teach_win32_Control1.cpp
@@ -144,6 +144,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     {
 	case WM_RBUTTONDOWN:
 	{
+		//对话框创建方法 对话框有没有动态创建的方法，待解决
 		g_hDlogWnd=CreateDialog(hInst, MAKEINTRESOURCE(IDD_DIALOG1),hWnd,myDialogProc );//创建一个非阻塞的对话框，返回HWMD
 		ShowWindow(g_hDlogWnd, SW_SHOWNORMAL);//创建非阻塞对话框之后要用showWindow显示
 		/*
@@ -157,10 +158,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_LBUTTONDOWN:
 	{
 		//MessageBox(hWnd, 0, 0, 0);//阻塞/同步模式，表示只有在当前对话框操作完成后才能操作主窗口
+		//静态对话框创建方法
 		DialogBox(hInst,//应用程序实例句柄
 			MAKEINTRESOURCE(IDD_DIALOG1),//控件名：通过对话框的ID利用MAKEINTRESOURCE()函数制造一个名字
 			hWnd,//父创就句柄
-			myDialogProc//对话框的消息处理函数，回调函数
+			About//对话框的消息处理函数，回调函数
 		);//弹出一个阻塞菜单
 		/*
 		INT_PTR WINAPI DialogBox(
@@ -225,16 +227,116 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     }
     return (INT_PTR)FALSE;//没有操作返回FALSE
 }
+TCHAR str[1024] = { 0 };
 //自己的对话框处理函数
 INT_PTR CALLBACK myDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	UNREFERENCED_PARAMETER(lParam);//安全等级
 	switch (message)
 	{
-	case WM_INITDIALOG://初始化对话框（对比WM_CREATE）
-		return (INT_PTR)TRUE;
+	case WM_INITDIALOG://初始化对话框时(参照WM_CREATE)，动态控件创建方法
+	{
+		//控件也是一个窗口
+		//下压式按钮
+		CreateWindow(_T("Button"),//是控件类名,其中Button是一个基类
+			_T("生成"),//控件窗口名
+			WS_CHILD | WS_VISIBLE|BS_PUSHBUTTON,//子窗口|可见|使用基类button的一个风格
+			10, 10,//控件位置
+			100, 30,//宽x，高y
+			hDlg,//控件的父窗口句柄
+			(HMENU)5001,//系统设置的控件ID号,对比静态控件里面的控件ID号
+			hInst,//实例句柄
+			nullptr//辅助信息 
+		);
+		//组框：显示信息 组框的基类也是Button，可以f12查看派生类类型
+		CreateWindow(_T("Button"),_T("调察问卷"),WS_CHILD | WS_VISIBLE | BS_GROUPBOX,10, 50,650, 200,hDlg,(HMENU)5002,hInst,nullptr);
+		//一栏：成员
+		//静态文本：给用户显示信息 基类Static，基类名可以在控件工具箱中查看
+		CreateWindow(_T("Static"), _T("家庭成员"), WS_CHILD | WS_VISIBLE , 20, 70, 60, 20, hDlg, (HMENU)5003, hInst, nullptr);
+		//单选框，基类Button，派生选BS_AUTORADIOBUTTON 分组为WS_GROUP 直到下个WS_GROUP前这些单选框为一组
+		CreateWindow(_T("Button"), _T("宝宝"), WS_CHILD | WS_VISIBLE| BS_AUTORADIOBUTTON|WS_GROUP, 20, 90, 50, 30, hDlg, (HMENU)6001, hInst, nullptr);
+		CreateWindow(_T("Button"), _T("爸爸"), WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 20, 120, 50, 30, hDlg, (HMENU)6002, hInst, nullptr);
+		CreateWindow(_T("Button"), _T("妈妈"), WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 20, 150, 50, 30, hDlg, (HMENU)6003, hInst, nullptr);
+		//二栏：学历
+		//静态文本：给用户显示信息 基类Static，基类名可以在控件工具箱中查看
+		CreateWindow(_T("Static"), _T("学历"), WS_CHILD | WS_VISIBLE, 300, 70, 60, 20, hDlg, (HMENU)5004, hInst, nullptr);
+		//单选框，基类Button，派生选BS_AUTORADIOBUTTON 分组为WS_GROUP 直到下个WS_GROUP前这些单选框为一组
+		CreateWindow(_T("Button"), _T("大专"), WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON | WS_GROUP, 300, 90, 50, 30, hDlg, (HMENU)6004, hInst, nullptr);
+		CreateWindow(_T("Button"), _T("本科"), WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 300, 120, 50, 30, hDlg, (HMENU)6005, hInst, nullptr);
+		CreateWindow(_T("Button"), _T("研究生"), WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 300, 150, 70, 30, hDlg, (HMENU)6006, hInst, nullptr);
+		//三栏：兴趣爱好
+		//静态文本：给用户显示信息 基类Static，基类名可以在控件工具箱中查看
+		CreateWindow(_T("Static"), _T("兴趣爱好"), WS_CHILD | WS_VISIBLE, 560, 70, 60, 20, hDlg, (HMENU)5005, hInst, nullptr);
+		//复选框，基类Button，派生选BS_AUTOCHECKBOX 
+		CreateWindow(_T("Button"), _T("画画"), WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX , 560, 90, 50, 30, hDlg, (HMENU)6007, hInst, nullptr);
+		CreateWindow(_T("Button"), _T("看书"), WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX, 560, 120, 50, 30, hDlg, (HMENU)6008, hInst, nullptr);
+		CreateWindow(_T("Button"), _T("写代码"), WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX, 560, 150, 70, 30, hDlg, (HMENU)6009, hInst, nullptr);
+
+		//编辑框，输入文字，基类：edit  风格WS_BORDER为边框 多行怎么改风格 multiline设置多行 待解决已解决ES_MULTILINE
+		CreateWindow(_T("Edit"), _T("统计结果"), WS_CHILD | WS_VISIBLE|WS_BORDER| ES_MULTILINE, 10, 260, 200, 100, hDlg, (HMENU)6010, hInst, nullptr);
+		//调察问卷的结果怎么保存或着说是录入，待解决
+
+		//设置默认的控件
+		//可以通过向系统投递消息来完成SendMessage()递归  PostMessage()同步
+		//上面并没有主动地设置变量来保存子控件的窗口句柄，因为子控件很多，仅仅通过窗口句柄很烦
+		//使用GetDlgItem来获得控件的句柄，投递BM_SETCHECK消息来设置默认的选项
+		SendMessage(GetDlgItem(hDlg, 6001), BM_SETCHECK, 1, 0);
+			/*获得控件的句柄
+			HWND GetDlgItem(
+			HWND hDlg,//哪一个对话框
+			int  nIDDlgItem//控件ID
+			);
+			*/
+		/*向系统投递消息
+		LRESULT SendMessage(
+		HWND   hWnd,
+		UINT   Msg,
+		WPARAM wParam,//一般的消息辅助鼠标消息放在wParam里面 ，1表示点击
+		LPARAM lParam
+		);
+		*/
+		SendMessage(GetDlgItem(hDlg, 6004), BM_SETCHECK, 1, 0);
+	}
+	return (INT_PTR)TRUE;
 
 	case WM_COMMAND://控件命令消息
+		switch (LOWORD(wParam))//响应鼠标点击消息,对应控件ID号
+		{
+		case 5001://选中这个控件时执行
+			
+			//---第一栏统计
+			_tcscpy_s(str, _T("家庭成员:"));//向字符串中拷贝一行文字
+			if (IsDlgButtonChecked(hDlg, 6001))//IsDlgButtonChecked()判断某一个按钮是否被选中
+				_tcscat(str, _T("宝宝"));
+			else if (IsDlgButtonChecked(hDlg, 6002))
+				_tcscat(str, _T("爸爸"));
+			else if (IsDlgButtonChecked(hDlg, 6003))
+				_tcscat(str, _T("妈妈"));
+			//---第二栏统计
+			_tcscat(str, _T("  学历："));
+			if (IsDlgButtonChecked(hDlg, 6004))
+				_tcscat(str, _T("大专"));
+			else if (IsDlgButtonChecked(hDlg, 6005))
+				_tcscat(str, _T("本科"));
+			else if (IsDlgButtonChecked(hDlg, 6006))
+				_tcscat(str, _T("研究生"));
+			//---第三栏统计
+			_tcscat(str, _T("  兴趣爱好："));
+			if (IsDlgButtonChecked(hDlg, 6007))
+				_tcscat(str, _T("画画 "));
+			if (IsDlgButtonChecked(hDlg, 6008))
+				_tcscat(str, _T("看书 "));
+			if (IsDlgButtonChecked(hDlg, 6009))
+				_tcscat(str, _T("写代码 "));
+
+			//把统计出来的消息发送到对话框中
+			SetDlgItemText(hDlg, 6010, str);
+			//成功生成
+			MessageBox(0, _T("成功生成"), 0, 0);
+			SetDlgItemText(hDlg, 6005, _T("更改"));//通过点一下生成后某个控件的名字标签改动
+			break;
+		}
+
 		if (LOWORD(wParam) == IDOK /*确定*/ || LOWORD(wParam) == IDCANCEL)
 		{
 			DestroyWindow(g_hDlogWnd);//非阻塞对话框释放方式
@@ -247,4 +349,4 @@ INT_PTR CALLBACK myDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lPa
 //控件也是一个窗口(子窗口，必须有一个父窗口)
 //控件也可以有消息
 //控件的窗口类已经在系统中定义害了，直接用
-//多种控件按钮在视图--工具箱中有（有对齐操作）
+//多种控件按钮在视图--工具箱中有（有对齐操作） 静态添加方式
